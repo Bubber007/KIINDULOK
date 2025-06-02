f28ae844d9928ed0e1470d9c352810c60f0ba8db
Bubber007 <balint.lipot@gmail.com>
2025-06-02 19:10:31 +0200

    Add files via upload

---
+++ Fazekas_Balint_C_Kiindulo/Horvath_Mark_Ckiindulo/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Horvath_Mark_Ckiindulo
+{
+    public class AppContext : DbContext
+    {
+        //public DbSet<Pilot> pilots { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=ADATBAZISNEVE;Uid=root;Pwd=;", ServerVersion.AutoDetect("Server=localhost;Database=ADATBAZISNEVE;Uid=root;Pwd=;"));
+        }
+
+    }
+}
---
+++ Fazekas_Balint_C_Kiindulo/Horvath_Mark_Ckiindulo/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Horvath_Mark_Ckiindulo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly AppContext context = new AppContext();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string tulajdonsagNev)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(tulajdonsagNev));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            //context.valami.load();
+            //Pilots = context.pilots.Local.ToObservableCollection();
+        }
+    }
+}
